53b124a582c0523755a1b711b18d539d4e865390
Rogerio Carvalho <rcarvalho@atlassian.com>
2018-08-19 18:06:31 +1000

    add comment to index js file about ES module loader for Node

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+/*
+* Adds ESM which is ES module loader for Node.
+* Reference: https://github.com/standard-things/esm
+*/
 require = require("esm")(module/*, options*/)
 module.exports = require("./main.js")
